bb48ab5e6575488661a5559820733fcf726c66b1
Aghakarim-dev <hemidzade1010@gmail.com>
2025-04-07 16:46:13 +0400

    added completed Task List

--- script.js
+++ script.js
@@ -1,54 +1,111 @@
 const input = document.querySelector("input");
 const taskList = document.getElementById("taskList");
+const completedTaskList = document.getElementById("completedTaskList");
+
 let tasks = [];
+let completedTasksArray = [];
+
+
+let unCompletedTasks = document.getElementById("unCompletedTasks")
+let completedTasks = document.getElementById("completedTasks")
+const lengthUnCompletedTasks = tasks.length
+const lengthCompletedTasks = completedTasksArray.length
 
 function addTask() {
-  let inputValue = input.value;
-
-  const newTask = {
-    id: Date.now(),
-    text: inputValue,
-  };
-
-  if (inputValue.trim() === "") {
-    alert("You must add a task");
-    return;
-  }
-
-  tasks.push(newTask);
-  input.value = "";
-
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  renderTask();
+    let inputValue = input.value;
+    
+    const newTask = {
+        id: Date.now(),
+        text: inputValue,
+    };
+    
+    if (inputValue.trim() === "") {
+        alert("You must add a task");
+        return;
+    }
+    
+    tasks.push(newTask);
+    input.value = "";
+    
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    
+    renderTask();
 }
 
 function renderTask() {
+
   if (tasks.length === 0) {
-    taskList.innerHTML = `<p class="text-center text-red-400 mt-4">You haven't add task yet</p>`;
+    taskList.innerHTML = `<p class="text-center text-red-400 mt-4">You haven't add a task yet</p>`;
   } else {
     taskList.innerHTML = tasks
       .map(
         (task) => `
             <li class="w-full flex border rounded-r-lg overflow-hidden h-[3rem]">
                 <p class="w-[70%] pl-2 h-full flex items-center">${task.text}</p>
-                <button onclick="deleteTask(${task.id})" class="w-[30%] flex justify-center items-center bg-red-500 h-full hover:cursor-pointer border-l">Delete</button>
+                <button onclick="completeTask(${task.id})" class="w-[15%] flex justify-center items-center h-full hover:cursor-pointer border-l">
+                    <i class="fa-solid fa-circle-check text-gray-500 hover:cursor-pointer"></i>
+                </button>
+                <button onclick="deleteTask(${task.id})" class="w-[15%] flex justify-center items-center bg-red-500 h-full hover:cursor-pointer text-[14px] border-l">Delete</button>
             </li>
             `
       )
       .join("");
   }
-}
+//   unCompletedTasks.textContent = `Uncompleted tasks: ${tasks.length}`
+//   completedTasks.textContent = `Completed tasks: ${completedTasksArray.length}`
+
+};
+
+function completeTask (id) {
+  let completedTask = tasks.find((item) => item.id === id)
+
+    if(completedTask) {
+        completedTasksArray.push(completedTask);
+
+        tasks = tasks.filter(item => item.id !== id);
+
+        completedTaskList.innerHTML =  taskList.innerHTML = completedTasksArray
+      .map(
+        (task) => `
+            <li class="w-full flex border rounded-r-lg overflow-hidden h-[3rem]">
+                <p class="w-[70%] pl-2 h-full flex items-center line-through text-gray-400">${task.text}</p>
+                <button onclick="completeTask(${task.id})" class="w-[15%] flex justify-center items-center h-full hover:cursor-pointer border-l">
+                    <i class="fa-solid fa-circle-check text-green-500 hover:cursor-pointer"></i>
+                </button>
+                <button onclick="deleteTask(${task.id})" class="w-[15%] flex justify-center items-center bg-red-500 h-full hover:cursor-pointer text-[14px] border-l">Delete</button>
+            </li>
+            `
+      )
+      .join("");
+
+        localStorage.setItem("completedTasks", JSON.stringify(completedTasksArray));
+        renderTask();   
+    }
+
+};
+
+
 
 function deleteTask(id) {
   tasks = tasks.filter((task) => task.id !== id);
   localStorage.setItem("tasks", JSON.stringify(tasks));
   renderTask();
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   const savedTasks = localStorage.getItem("tasks");
   if (savedTasks) {
     tasks = JSON.parse(savedTasks);
   }
+
+  const completedTasks = localStorage.getItem("completedTasks");
+  if(completedTasks) {
+    completedTasksArray = JSON.parse(completedTasks)
+  }
+
   renderTask();
+
 });
+
+
+
