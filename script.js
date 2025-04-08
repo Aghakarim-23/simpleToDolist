4bd2ea53b29b998e25f187f5d296890b7410ed65
Aghakarim-dev <hemidzade1010@gmail.com>
2025-04-08 09:40:06 +0400

    fixed completed and delete buttons

--- script.js
+++ script.js
@@ -5,35 +5,33 @@ const completedTaskList = document.getElementById("completedTaskList");
 let tasks = [];
 let completedTasksArray = [];
 
-
-let unCompletedTasks = document.getElementById("unCompletedTasks")
-let completedTasks = document.getElementById("completedTasks")
-const lengthUnCompletedTasks = tasks.length
-const lengthCompletedTasks = completedTasksArray.length
+let unCompletedTasks = document.getElementById("unCompletedTasks");
+let completedTasks = document.getElementById("completedTasks");
+const lengthUnCompletedTasks = tasks.length;
+const lengthCompletedTasks = completedTasksArray.length;
 
 function addTask() {
-    let inputValue = input.value;
-    
-    const newTask = {
-        id: Date.now(),
-        text: inputValue,
-    };
-    
-    if (inputValue.trim() === "") {
-        alert("You must add a task");
-        return;
-    }
-    
-    tasks.push(newTask);
-    input.value = "";
-    
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    
-    renderTask();
+  let inputValue = input.value;
+
+  const newTask = {
+    id: Date.now(),
+    text: inputValue,
+  };
+
+  if (inputValue.trim() === "") {
+    alert("You must add a task");
+    return;
+  }
+
+  tasks.push(newTask);
+//   input.value = "";
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  renderTask();
 }
 
 function renderTask() {
-
   if (tasks.length === 0) {
     taskList.innerHTML = `<p class="text-center text-red-400 mt-4">You haven't add a task yet</p>`;
   } else {
@@ -51,20 +49,20 @@ function renderTask() {
       )
       .join("");
   }
-//   unCompletedTasks.textContent = `Uncompleted tasks: ${tasks.length}`
-//   completedTasks.textContent = `Completed tasks: ${completedTasksArray.length}`
+  unCompletedTasks.textContent = `Uncompleted tasks: ${tasks.length}`;
+  completedTasks.textContent = `Completed tasks: ${completedTasksArray.length}`;
 
-};
+}
 
-function completeTask (id) {
-  let completedTask = tasks.find((item) => item.id === id)
+function completeTask(id) {
+  let completedTask = tasks.find((item) => item.id === id);
 
-    if(completedTask) {
-        completedTasksArray.push(completedTask);
+  if (completedTask) {
+    completedTasksArray.push(completedTask);
 
-        tasks = tasks.filter(item => item.id !== id);
+    tasks = tasks.filter((item) => item.id !== id);
 
-        completedTaskList.innerHTML =  taskList.innerHTML = completedTasksArray
+    completedTaskList.innerHTML = completedTasksArray
       .map(
         (task) => `
             <li class="w-full flex border rounded-r-lg overflow-hidden h-[3rem]">
@@ -72,25 +70,58 @@ function completeTask (id) {
                 <button onclick="completeTask(${task.id})" class="w-[15%] flex justify-center items-center h-full hover:cursor-pointer border-l">
                     <i class="fa-solid fa-circle-check text-green-500 hover:cursor-pointer"></i>
                 </button>
-                <button onclick="deleteTask(${task.id})" class="w-[15%] flex justify-center items-center bg-red-500 h-full hover:cursor-pointer text-[14px] border-l">Delete</button>
+                <button onclick="deleteCompletedTask(${task.id})" class="w-[15%] flex justify-center items-center bg-red-500 h-full hover:cursor-pointer text-[14px] border-l">Delete</button>
             </li>
             `
       )
       .join("");
-
-        localStorage.setItem("completedTasks", JSON.stringify(completedTasksArray));
-        renderTask();   
-    }
-
-};
-
+      
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("completedTasksArray", JSON.stringify(completedTasksArray));
+    renderTask();
+    // localStorage.clear("tasks")
+  }
+  
+}
 
 
 function deleteTask(id) {
   tasks = tasks.filter((task) => task.id !== id);
   localStorage.setItem("tasks", JSON.stringify(tasks));
   renderTask();
-};
+}
+
+function deleteCompletedTask (id) {
+    const completedTaskDeleteItem = completedTasksArray.find((item => item.id === id))
+    if(completedTaskDeleteItem) {
+        console.log(completedTaskDeleteItem);
+
+        completedTasksArray = completedTasksArray.filter(item => item.id !== id)
+    }
+
+    localStorage.setItem("completedTasksArray", JSON.stringify(completedTasksArray))
+    renderTask()
+    renderCompletedTask()    
+}
+
+
+function renderCompletedTask () {
+    completedTaskList.innerHTML = completedTasksArray.map(task => 
+            `
+            <li class="w-full flex border rounded-r-lg overflow-hidden h-[3rem]">
+                <p class="w-[70%] pl-2 h-full flex items-center line-through text-gray-400">${task.text}</p>
+                <button onclick="completeTask(${task.id})" class="w-[15%] flex justify-center items-center h-full hover:cursor-pointer border-l">
+                    <i class="fa-solid fa-circle-check text-green-500 hover:cursor-pointer"></i>
+                </button>
+                <button onclick="deleteCompletedTask(${task.id})" class="w-[15%] flex justify-center items-center bg-red-500 h-full hover:cursor-pointer text-[14px] border-l">Delete</button>
+            </li>
+            `
+      )
+      .join("");
+
+        
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const savedTasks = localStorage.getItem("tasks");
@@ -98,14 +129,13 @@ document.addEventListener("DOMContentLoaded", () => {
     tasks = JSON.parse(savedTasks);
   }
 
-  const completedTasks = localStorage.getItem("completedTasks");
-  if(completedTasks) {
-    completedTasksArray = JSON.parse(completedTasks)
-  }
+  const completedTasks = localStorage.getItem("completedTasksArray"); 
+  if (completedTasks) {
+    completedTasksArray = JSON.parse(completedTasks);
+}
 
-  renderTask();
-
+renderTask();
+renderCompletedTask()
 });
-
 
 
